5763de8134bd2d2cf1d1ab354f826d111551a201
Alex-Catalin21 <alex_catalin_2009@yahoo.com>
2022-06-27 10:42:57 +0300

    added password validation with regex

--- register-user.php
+++ register-user.php
@@ -16,6 +16,20 @@
             if(isset($_POST["remail"], $_POST["rpass"])){
             $email = $_POST["remail"];
             $password = $_POST["rpass"];
+
+            //password  validation with regex
+            $uppercase = preg_match('@[A-Z]@', $password);
+            $lowercase = preg_match('@[a-z]@', $password);
+            $number = preg_match('@[0-9]@', $password);
+            $specialChars = preg_match('@[^\w]@',$password);
+
+            if(!$uppercase || !$lowercase || !$number || 
+            !$specialChars || strlen($password) < 8){
+                echo 'Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.';
+            }else{
+                echo 'Strong password.';
+            
+
             $hash = password_hash($password, PASSWORD_DEFAULT);
             if($cerere -> execute ([
                 'email' => $email,
@@ -25,7 +39,7 @@
                     // Redirect user to login page
                     header("location: login.php");
             };
-            }else{
+            }}else{
 
                 
                 session_start();
